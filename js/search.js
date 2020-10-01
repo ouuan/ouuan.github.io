3fa7fed8f6a5238ab3cac59c2362efb0c5f63618
ouuan <ouuan@users.noreply.github.com>
2020-10-01 00:52:52 +0000

    fix(others): fix image size on Firefox ouuan/hugo-blog@ed6e3bd224410963f35c81410566a774b7c9c2cb

--- js/search.js
+++ js/search.js
@@ -3,30 +3,30 @@ var fuseOptions = {
   shouldSort: true,
   includeMatches: true,
   threshold: 0.1,
-  tokenize:true,
+  tokenize: true,
   location: 0,
   distance: 100000,
   maxPatternLength: 32,
   minMatchCharLength: 1,
   keys: [
-    {name:"title",weight:0.9},
-    {name:"contents",weight:0.8},
-    {name:"tags",weight:0.5},
-    {name:"categories",weight:0.3}
+    { name: "title", weight: 0.9 },
+    { name: "contents", weight: 0.8 },
+    { name: "tags", weight: 0.5 },
+    { name: "categories", weight: 0.3 }
   ]
 };
 
-function executeSearch(){
+function executeSearch() {
   $('#search-results').html('正在搜索中...');
   searchContent = $('#search-content')[0].value;
-  if (searchContent){
-    $.getJSON( "/index.json", function( data ) {
+  if (searchContent) {
+    $.getJSON("/index.json", function (data) {
       var pages = data;
       var fuse = new Fuse(pages, fuseOptions);
       var result = fuse.search(searchContent);
-      if(result.length > 0){
+      if (result.length > 0) {
         populateResults(result, searchContent);
-      }else{
+      } else {
         $('#search-results').html("<p>没有找到您想要的结果。要不试试缩短关键词？</p>");
       }
     });
@@ -35,10 +35,10 @@ function executeSearch(){
   }
 }
 
-function populateResults(result, searchContent){
+function populateResults(result, searchContent) {
   $('#search-results').html('');
-  $.each(result,function(key,value){
-    var contents= value.item.contents;
+  $.each(result, function (key, value) {
+    var contents = value.item.contents;
     var snippet = "";
     var snippetHighlights = [searchContent];
 
@@ -53,49 +53,49 @@ function populateResults(result, searchContent){
       snippet = contents.substring(start, end);
     }
 
-    $.each(value.matches,function(matchKey,mvalue) {
+    $.each(value.matches, function (matchKey, mvalue) {
       start = mvalue.indices[0][0] - summaryInclude;
       end = mvalue.indices[0][1] + summaryInclude + 1;
       if (start < 0) start = 0;
       if (end > mvalue.value.length) end = mvalue.value.length;
       if (pos == -1 && mvalue.key == "contents") {
-        snippet += mvalue.value.substring(start,end);
+        snippet += mvalue.value.substring(start, end);
       }
-      snippetHighlights.push(mvalue.value.substring(mvalue.indices[0][0],mvalue.indices[0][1] + 1));
+      snippetHighlights.push(mvalue.value.substring(mvalue.indices[0][0], mvalue.indices[0][1] + 1));
     });
 
-    if(snippet.length<1){
-      snippet += contents.substring(0,summaryInclude*2);
+    if (snippet.length < 1) {
+      snippet += contents.substring(0, summaryInclude * 2);
     }
     //pull template from hugo templarte definition
     var templateDefinition = $('#search-result-template').html();
     //replace values
-    var output = render(templateDefinition,{key:key,title:value.item.title,link:value.item.permalink,tags:value.item.tags,categories:value.item.categories,snippet:snippet, date:value.item.date.split('T')[0]});
+    var output = render(templateDefinition, { key: key, title: value.item.title, link: value.item.permalink, tags: value.item.tags, categories: value.item.categories, snippet: snippet, date: value.item.date.split('T')[0] });
 
     $('#search-results').append(output);
 
-    $.each(snippetHighlights,function(snipkey,snipvalue){
+    $.each(snippetHighlights, function (snipkey, snipvalue) {
       new Mark(document.getElementById("summary-" + key)).mark(snipvalue);
     });
   });
 }
 
 function param(name) {
-    return decodeURIComponent((location.search.split(name + '=')[1] || '').split('&')[0]).replace(/\+/g, ' ');
+  return decodeURIComponent((location.search.split(name + '=')[1] || '').split('&')[0]).replace(/\+/g, ' ');
 }
 
 function render(templateString, data) {
-  var conditionalMatches,conditionalPattern,copy;
+  var conditionalMatches, conditionalPattern, copy;
   conditionalPattern = /\$\{\s*isset ([a-zA-Z]*) \s*\}(.*)\$\{\s*end\s*}/g;
   //since loop below depends on re.lastInxdex, we use a copy to capture any manipulations whilst inside the loop
   copy = templateString;
   while ((conditionalMatches = conditionalPattern.exec(templateString)) !== null) {
-    if(data[conditionalMatches[1]]){
+    if (data[conditionalMatches[1]]) {
       //valid key, remove conditionals, leave contents.
-      copy = copy.replace(conditionalMatches[0],conditionalMatches[2]);
-    }else{
+      copy = copy.replace(conditionalMatches[0], conditionalMatches[2]);
+    } else {
       //not valid, remove entire section
-      copy = copy.replace(conditionalMatches[0],'');
+      copy = copy.replace(conditionalMatches[0], '');
     }
   }
   templateString = copy;
